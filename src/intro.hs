679dd9ef1a52c4ff8bf14f4275140daa293ac8e9
tgeeky <technogeeky+github@gmail.com>
2012-06-30 16:00:51 -0400

    compiles

--- src/intro.hs
+++ src/intro.hs
@@ -1,17 +1,25 @@
+{-# LANGUAGE UnicodeSyntax #-}
+
+import Data.List 
+               ( unfoldr   {-  unfoldr   ::        (b → Maybe (a,b)) → b → [a]         -}
+               , partition {-  partition ::        (a -> Bool) -> [a] -> ([a], [a])    -}
+               , delete    {-  delete    :: Eq a => a -> [a] -> [a]                    -}
+               )
+
 insertSort :: [Integer] → [Integer]
 insertSort = foldr insert [ ]
 
 insert :: Integer → [Integer] → [Integer]
 insert y ys = xs ++ [y] ++ zs
-where (xs,zs) = partition (<y) ys
+     where (xs,zs) = partition (<y) ys
 
-unfoldr :: (b → Maybe (a,b)) → b → [a]
 
 selectSort :: [Integer] → [Integer]
 selectSort = unfoldr select
 
 select :: [Integer] → Maybe (Integer,[Integer])
 select [ ] = Nothing
-select xs = Just (x,xs0)
- where x   = minimum xs xs0 = delete x xs
+select xs= Just (x,xs0)
+  where x   = minimum xs 
+        xs0 = delete x xs
 
